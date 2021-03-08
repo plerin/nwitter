8b66916d8b16dd798d30e1504de93dbcf772b2d5
con <plerin152@gmail.com>
2021-03-08 10:47:57 +0900

    modify the code
    Auth.js, incoming code

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,38 +1,10 @@
-<<<<<<< HEAD
-=======
 import { authService, firebaseInstance } from "fbase";
->>>>>>> t14
 import React, { useState } from "react";
 
 // export default () => <span>Auth</span>
 
 // 다른 파일에서 자동 import 할 수 있는 방법
 const Auth = () => {
-<<<<<<< HEAD
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const onChange = (event) => {
-        console.log(event.target.name);
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-    };
-    return (
-        <div>
-        <form onSubmit={onSubmit}>
-            <input name="email" type="text" placeholder="Email" value={email} onChange={onChange} required />
-            <input name="password" type="password" placeholder="Password" value={password} onChange={onChange} required />
-            <input type="submit" value="Log In" />
-        </form>
-        <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
-        </div>
-    </div>
-    );
-}
-export default Auth;
-=======
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setnewAccount] = useState(true);
@@ -112,4 +84,3 @@ export default Auth;
   );
 };
 export default Auth;
->>>>>>> t14
